ecb1fcee4d2c00f4d5dd3d45cce7738f22a7cee1
maxnatalia <Nataliamazur1988@gmail.com>
2022-11-15 09:27:43 +0100

    added background-color for body

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -14,5 +14,6 @@ export const GlobalStyle = createGlobalStyle`
 
     body {
         font-family: 'Poppins', sans-serif; 
+        background-color: ${({ theme }) => theme.color.mercury};
     }
 `;
