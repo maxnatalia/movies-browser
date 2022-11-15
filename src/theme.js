8e46c8d224954d97cc0c111bd51d50ce53d2d08d
maxnatalia <Nataliamazur1988@gmail.com>
2022-11-15 09:28:35 +0100

    added basic elements - color, breakpoint

---
+++ src/theme.js
@@ -0,0 +1,19 @@
+export const theme = {
+    color: {
+        mercury: "#E5E5E5",
+        white: "#FFFFFF",
+        black: "#000000",
+        lightBlack: "#18181B",
+        blue: "#0044CC",
+        lightBlue: "#D6E4FF",
+        grey: "#E4E6F0",
+        darkerGrey: "#7E839A",
+        stormGrey: "#74788B",
+    },
+
+    breakpoint: {
+        //for max-width:
+        mobile: 767,
+        tablet: 1199,
+    }
+};
