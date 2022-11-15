9a48d738c73a5eabeb68c702a93e2eacd4052bee
golosgit <107067096+golosgit@users.noreply.github.com>
2022-11-15 22:04:09 +0100

    added 2 colors

--- src/theme.js
+++ src/theme.js
@@ -2,9 +2,11 @@ export const theme = {
     color: {
         mercury: "#E5E5E5",
         white: "#FFFFFF",
+        whisper: "#F5F5FA",
         black: "#000000",
         lightBlack: "#18181B",
         blue: "#0044CC",
+        blueRibbon: "#0055FF",
         lightBlue: "#D6E4FF",
         grey: "#E4E6F0",
         darkerGrey: "#7E839A",
