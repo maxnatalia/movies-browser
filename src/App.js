6adf891a0f12b4d87aa75cdf383c2bbaecdf4130
Patryk Krawczyk <patrick36212@gmail.com>
2022-11-16 20:23:45 +0100

    add component to app

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { ThemeProvider } from "styled-components";
 import { theme } from "./theme";
 import { GlobalStyle } from "./GlobalStyle";
 import MoviesList from "./features/movies/MoviesList";
+import Navigation from "./common/Navigation";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
+      <Navigation />
       <MoviesList />
     </ThemeProvider>
   );
