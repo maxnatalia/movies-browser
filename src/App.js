bf8393144bbfd2283535a7c538d3aeb5b0eaf6ab
maxnatalia <Nataliamazur1988@gmail.com>
2022-11-17 12:16:24 +0100

    added new component PeopleList

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { ThemeProvider } from "styled-components";
 import { theme } from "./theme";
 import { GlobalStyle } from "./GlobalStyle";
-import MoviesList from "./features/movies/MoviesList";
+// import MoviesList from "./features/movies/MoviesList";
+// import Navigation from "./Navigation/index";
+import PeopleList from "./features/people/PeopleList";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <MoviesList />
+      <PeopleList />
     </ThemeProvider>
   );
 };
--- src/features/people/PeopleList/index.js
+++ src/features/people/PeopleList/index.js
@@ -1,6 +1,56 @@
+import { MainWrapper, Header, TilesContainer, TilePerson, ImageWrapper, Image, Title } from "./styled";
+import poster from "../../../common/images/posterPerson.png";
+
 const PeopleList = () => {
     return (
-        <div>PeopleList</div>
+
+        <MainWrapper>
+            <Header>Popular people</Header>
+            <TilesContainer>
+                <TilePerson>
+                    <ImageWrapper>
+                        <Image src={poster} alt="" />
+                    </ImageWrapper>
+                    <Title>Lui Yifey</Title>
+                </TilePerson>
+                <TilePerson>
+                    <ImageWrapper>
+                        <Image />
+                    </ImageWrapper>
+                    <Title>Lui Yifey</Title>
+                </TilePerson>
+                <TilePerson>
+                    <ImageWrapper>
+                        <Image />
+                    </ImageWrapper>
+                    <Title>Lui Yifey hehehehehe hj</Title>
+                </TilePerson>
+                <TilePerson>
+                    <ImageWrapper>
+                        <Image />
+                    </ImageWrapper>
+                    <Title>Lui Yifey hehehehehe h</Title>
+                </TilePerson>
+                <TilePerson>
+                    <ImageWrapper>
+                        <Image />
+                    </ImageWrapper>
+                    <Title>Lui Yifey</Title>
+                </TilePerson>
+                <TilePerson>
+                    <ImageWrapper>
+                        <Image />
+                    </ImageWrapper>
+                    <Title>Lui Yifey</Title>
+                </TilePerson>
+                <TilePerson>
+                    <ImageWrapper>
+                        <Image />
+                    </ImageWrapper>
+                    <Title>Lui Yifey</Title>
+                </TilePerson>
+            </TilesContainer>
+        </MainWrapper>
     )
 };
 
