65a1880a9438c862d9bf1517b741430cd95f8329
maxnatalia <Nataliamazur1988@gmail.com>
2022-11-23 09:41:58 +0100

    added watchFetchPopularMovies to rootSaga

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,8 +1,8 @@
 import { all } from "redux-saga/effects";
+import { watchFetchPopularMovies } from "./features/movies/moviesSaga";
 
 export default function* rootSaga() {
     yield all([
-        // moviesSaga(),
-        // peopleSaga(),
+        watchFetchPopularMovies(),
     ])
 };
