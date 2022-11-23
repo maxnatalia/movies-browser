d8ddacd94ab7f7a37ade1a095a920cc3682c6d35
maxnatalia <Nataliamazur1988@gmail.com>
2022-11-23 09:45:05 +0100

    added logic to component moviesList

--- src/features/movies/MoviesList/index.js
+++ src/features/movies/MoviesList/index.js
@@ -15,21 +15,33 @@ import {
     Rate,
     Votes
 } from "./styled";
-import poster from "../../../common/images/poster.png";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchMovies, selectMovies } from "../moviesSlice";
 
 const MoviesList = ({ insideDetails }) => {
+    const movies = useSelector(selectMovies);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchMovies());
+    }, [dispatch]);
+
     return (
-            <MainWrapper insideDetails={insideDetails}>
-                <Header>Popular movies</Header>
-                <TilesContainer insideDetails={insideDetails}>
-                    <TileMovie>
+        <MainWrapper insideDetails={insideDetails}>
+            <Header>Popular movies</Header>
+            <TilesContainer insideDetails={insideDetails}>
+                {movies.map(movie =>
+                    <TileMovie
+                        key={movie.id}
+                    >
                         <ImageWrapper>
-                            <Image src={poster} alt="" />
+                            <Image src={`https://image.tmdb.org/t/p/w400${movie.poster_path}`} alt="" />
                         </ImageWrapper>
                         <ContentWrapper>
-                            <Title>Mulan</Title>
+                            <Title>{movie.original_title}</Title>
                             <StyledDate>
-                                2020
+                                {movie.release_date.slice(0,4)}
                             </StyledDate>
                             <TagsWrapper>
                                 <Tag>Action</Tag>
@@ -38,91 +50,92 @@ const MoviesList = ({ insideDetails }) => {
                             </TagsWrapper>
                             <RatingWrapper>
                                 <StarIcon />
-                                <Rate>7,8</Rate>
-                                <Votes>35 votes</Votes>
+                                <Rate>{movie.vote_average}</Rate>
+                                <Votes>{movie.vote_count} votes</Votes>
                             </RatingWrapper>
                         </ContentWrapper>
                     </TileMovie>
-                    <TileMovie>
-                        <ImageWrapper>
-                            <Image />
-                        </ImageWrapper>
-                        <ContentWrapper>
-                            <Title>Mulan</Title>
-                            <StyledDate>
-                                2020
-                            </StyledDate>
-                            <TagsWrapper>
-                            </TagsWrapper>
-                            <RatingWrapper>
-                                <StarIcon />
-                                <Rate>7,8</Rate>
-                                <Votes>35 votes</Votes>
-                            </RatingWrapper>
-                        </ContentWrapper>
-                    </TileMovie>
-                    <TileMovie>
-                        <ImageWrapper>
-                            <Image />
-                        </ImageWrapper>
-                        <ContentWrapper>
-                            <Title>Mulan</Title>
-                            <StyledDate>
-                            </StyledDate>
-                            <TagsWrapper>
-                                <Tag>Action</Tag>
-                            </TagsWrapper>
-                            <RatingWrapper>
-                                <StarIcon />
-                                <Rate>7,8</Rate>
-                                <Votes>35 votes</Votes>
-                            </RatingWrapper>
-                        </ContentWrapper>
-                    </TileMovie>
-                    <TileMovie>
-                        <ImageWrapper>
-                            <Image />
-                        </ImageWrapper>
-                        <ContentWrapper>
-                            <Title>Mulanhshsjsskskgfddddddddddd</Title>
-                            <StyledDate>
-                                2020
-                            </StyledDate>
-                            <TagsWrapper>
-                                <Tag>Action</Tag>
-                                <Tag>Thriller</Tag>
-                                <Tag>Action</Tag>
-                            </TagsWrapper>
-                            <RatingWrapper>
-                                <StarIcon />
-                                <Rate>7,8</Rate>
-                                <Votes>35 votes</Votes>
-                            </RatingWrapper>
-                        </ContentWrapper>
-                    </TileMovie>
-                    <TileMovie>
-                        <ImageWrapper>
-                            <Image />
-                        </ImageWrapper>
-                        <ContentWrapper>
-                            <Title>Mulan</Title>
-                            <StyledDate>
-                                2020
-                            </StyledDate>
-                            <TagsWrapper>
-                                <Tag>Action</Tag>
-                                <Tag>Thriller</Tag>
-                                <Tag>Action</Tag>
-                            </TagsWrapper>
-                            <RatingWrapper>
-                                <StarIcon />
-                                <Rate>7,8</Rate>
-                                <Votes>35 votes</Votes>
-                            </RatingWrapper>
-                        </ContentWrapper>
-                    </TileMovie>
-                </TilesContainer>
-            </MainWrapper>
+                )}
+                {/* <TileMovie>
+                    <ImageWrapper>
+                        <Image />
+                    </ImageWrapper>
+                    <ContentWrapper>
+                        <Title>Mulan</Title>
+                        <StyledDate>
+                            2020
+                        </StyledDate>
+                        <TagsWrapper>
+                        </TagsWrapper>
+                        <RatingWrapper>
+                            <StarIcon />
+                            <Rate>7,8</Rate>
+                            <Votes>35 votes</Votes>
+                        </RatingWrapper>
+                    </ContentWrapper>
+                </TileMovie>
+                <TileMovie>
+                    <ImageWrapper>
+                        <Image />
+                    </ImageWrapper>
+                    <ContentWrapper>
+                        <Title>Mulan</Title>
+                        <StyledDate>
+                        </StyledDate>
+                        <TagsWrapper>
+                            <Tag>Action</Tag>
+                        </TagsWrapper>
+                        <RatingWrapper>
+                            <StarIcon />
+                            <Rate>7,8</Rate>
+                            <Votes>35 votes</Votes>
+                        </RatingWrapper>
+                    </ContentWrapper>
+                </TileMovie>
+                <TileMovie>
+                    <ImageWrapper>
+                        <Image />
+                    </ImageWrapper>
+                    <ContentWrapper>
+                        <Title>Mulanhshsjsskskgfddddddddddd</Title>
+                        <StyledDate>
+                            2020
+                        </StyledDate>
+                        <TagsWrapper>
+                            <Tag>Action</Tag>
+                            <Tag>Thriller</Tag>
+                            <Tag>Action</Tag>
+                        </TagsWrapper>
+                        <RatingWrapper>
+                            <StarIcon />
+                            <Rate>7,8</Rate>
+                            <Votes>35 votes</Votes>
+                        </RatingWrapper>
+                    </ContentWrapper>
+                </TileMovie>
+                <TileMovie>
+                    <ImageWrapper>
+                        <Image />
+                    </ImageWrapper>
+                    <ContentWrapper>
+                        <Title>Mulan</Title>
+                        <StyledDate>
+                            2020
+                        </StyledDate>
+                        <TagsWrapper>
+                            <Tag>Action</Tag>
+                            <Tag>Thriller</Tag>
+                            <Tag>Action</Tag>
+                        </TagsWrapper>
+                        <RatingWrapper>
+                            <StarIcon />
+                            <Rate>7,8</Rate>
+                            <Votes>35 votes</Votes>
+                        </RatingWrapper>
+                    </ContentWrapper>
+                </TileMovie> */}
+            </TilesContainer>
+        </MainWrapper>
     )
 };
 
