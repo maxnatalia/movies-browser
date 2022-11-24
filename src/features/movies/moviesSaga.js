4e1d505e5f96e85eea8b11630bbf1115e16face0
maxnatalia <Nataliamazur1988@gmail.com>
2022-11-24 07:57:52 +0100

    deleted comments and unnecessary payload

--- src/features/movies/moviesSaga.js
+++ src/features/movies/moviesSaga.js
@@ -1,12 +1,10 @@
-// import axios from "axios";
 import { takeLatest, put, call, delay } from "redux-saga/effects";
 import { fetchError, fetchMoviesSuccess, fetchMovies } from "./moviesSlice";
-// import { apiUrl, apiKey } from "../../apiData";
 import { getMovies } from "./api";
 
-function* fetchMoviesHandler({ payload }) {
+function* fetchMoviesHandler() {
     try {
-        const data = yield call(getMovies, payload);
+        const data = yield call(getMovies);
         const movies = yield data.results;
         yield delay(3000);
         yield put(
@@ -18,19 +16,6 @@ function* fetchMoviesHandler({ payload }) {
         yield put(fetchError());
     }
 }
-
-// function* fetchMovieHandler({ payload: id }) {
-//     try {
-//         const movie = yield axios.get(
-//             `${apiUrl}movie/${id}?api_key=${apiKey}&language=en-US`
-//         );
-//         yield put(fetchMovieSuccess({ payload: movie }));
-//     } catch (error) {
-//         yield put(fetchError());
-//     }
-// }
-
 export function* watchFetchPopularMovies() {
     yield takeLatest(fetchMovies.type, fetchMoviesHandler);
-    // yield takeLatest(fetchMovie.type, fetchMovieHandler);
 }
