a620f36d23fb492c2afb17dcead707bfbf0b1f50
golosgit <107067096+golosgit@users.noreply.github.com>
2022-11-26 17:30:26 +0100

    MovieDetails with logic (cast and vrew not working)

--- src/features/movies/MovieDetails/index.js
+++ src/features/movies/MovieDetails/index.js
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import MainWrapper from "../../../common/MainWrapper";
 import {
   DetailsImage,
@@ -22,20 +22,30 @@ import { PosterWrapper, PosterDisplay, MainInfoWrapper, MainInfoTitle } from "..
 import Loading from "../../../common/Loading";
 import Error from "../../../common/Error";
 import PeopleList from "../../people/PeopleList";
-import { changeMovieId, fetchMovieDetails, selectError, selectLoading } from "./movieDetailsSlice";
-import image from "../../../common/images/poster.png";
+import { changeMovieId, fetchMovieDetails, selectError, selectLoading, selectMovieDetails } from "./movieDetailsSlice";
+import { apiBackdrop } from "../../../apiData";
 
 const MovieDetails = () => {
   const dispatch = useDispatch();
   const { id } = useParams();
   const loading = useSelector(selectLoading);
   const error = useSelector(selectError);
+  const movie = useSelector(selectMovieDetails);
+  const releaseDate = movie.release_date;
+  const [date, setDate] = useState("");
 
   useEffect(() => {
     dispatch(changeMovieId(id));
     dispatch(fetchMovieDetails());
-  });
+  }, []);
 
+  useEffect(() => {
+    if (releaseDate) {
+      const [year, month, day] = releaseDate.split("-");
+      setDate(day + "." + month + "." + year);
+    }
+  }, [releaseDate]);
+  
   if (loading) {
     return <Loading />;
   } else if (error) {
@@ -45,59 +55,61 @@ const MovieDetails = () => {
   return (
     <>
       <PosterDisplay>
-        <PosterWrapper>
+        <PosterWrapper style={{ backgroundImage: `url(${apiBackdrop}${movie.backdrop_path})` }}>
           <MainInfoWrapper>
-            <MainInfoTitle>Mulan long title</MainInfoTitle>
+            <MainInfoTitle>{movie.title}</MainInfoTitle>
             <RatingWrapper>
               <StarIcon display="true" />
-              <Rate infoMain>7,8</Rate>
+              <Rate infoMain>{movie.vote_average.toFixed(1)}</Rate>
               <Votes infoMain>/ 10</Votes>
               <Votes infoMain normalHidden>
-                335 votes
+                {movie.vote_count} votes
               </Votes>
             </RatingWrapper>
             <RatingWrapper mobileHidden>
-              <Votes infoMain>335 votes</Votes>
+              <Votes infoMain>{movie.vote_count} votes</Votes>
             </RatingWrapper>
           </MainInfoWrapper>
         </PosterWrapper>
       </PosterDisplay>
       <MainWrapper>
         <DetailsWrapper>
-          <DetailsImage src={image} alt="" />
+          <DetailsImage src={`https://image.tmdb.org/t/p/w400${movie.poster_path}`} alt="" />
           <TextWrapper>
-            <DetailsTitle>Mulan</DetailsTitle>
-            <Year>2020</Year>
+            <DetailsTitle>{movie.title}</DetailsTitle>
+            <Year>{movie.release_date.slice(0, 4)}</Year>
             <AdditionalInfoWrapper>
               <DetailsText additionalQuestion mobileHidden>
                 Production:
               </DetailsText>
-              <DetailsText additionalAnswer>China, United States of America</DetailsText>
+              <DetailsText additionalAnswer>{movie.production_countries.map((country) => country.name)}</DetailsText>
             </AdditionalInfoWrapper>
             <AdditionalInfoWrapper secondLine>
               <DetailsText additionalQuestion mobileHidden>
                 Release date:
               </DetailsText>
-              <DetailsText additionalAnswer>24.10.2020</DetailsText>
+              <DetailsText additionalAnswer>
+                {date}
+              </DetailsText>
             </AdditionalInfoWrapper>
-            <TagsWrapper>
-              <Tag>Action</Tag>
-              <Tag>Adventure</Tag>
-              <Tag>Drama</Tag>
-            </TagsWrapper>
+            {movie.genres ? (
+              <TagsWrapper>
+                {movie.genres.map((genre) => (
+                  <Tag key={id}>{genre.name}</Tag>
+                ))}
+              </TagsWrapper>
+            ) : (
+              ""
+            )}
             <RatingWrapper mobile>
               <StarIcon />
-              <Rate>7,8</Rate>
+              <Rate>{movie.vote_average.toFixed(1)}</Rate>
               <Votes mobileHidden>/ 10</Votes>
-              <Votes mobile>335 votes</Votes>
+              <Votes mobile>{movie.vote_count} votes</Votes>
             </RatingWrapper>
           </TextWrapper>
           <Info>
-            <DetailsText>
-              A young Chinese maiden disguises herself as a male warrior in order to save her father. Disguises herself
-              as a male warrior in order to save her father. A young Chinese maiden disguises herself as a male warrior
-              in order to save her father.
-            </DetailsText>
+            <DetailsText>{movie.overview}</DetailsText>
           </Info>
         </DetailsWrapper>
         <PeopleList insideDetails />
