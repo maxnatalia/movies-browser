50ea92d5c81f76a30e6ff2ed8637208ec3452eb4
Patryk Krawczyk <patrick36212@gmail.com>
2022-11-26 16:20:20 +0100

    add query & event handler

--- src/common/Navigation/Search/index.js
+++ src/common/Navigation/Search/index.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { selectQuery, setQuery } from "../../../features/movies/moviesSlice";
 import { SearchIcon, SearchInput, SearchWrapper } from "./styled";
 
 const Search = () => {
+    const query = useSelector(selectQuery);
+    const dispatch = useDispatch()
+    const onChangeValue = (event) => dispatch(setQuery(event.target.value));
+
     return (
         <SearchWrapper>
             <SearchIcon />
             <SearchInput
+                value={query}
+                onChange={onChangeValue}
                 type="text"
                 placeholder="Search for movies..."
             />
