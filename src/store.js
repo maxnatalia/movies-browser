d85df11d1e3bd7b5126fee5c61af515dcce66bde
maxnatalia <Nataliamazur1988@gmail.com>
2022-11-27 09:50:41 +0100

    added peopleReducer to store.js

--- src/store.js
+++ src/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "@redux-saga/core";
 import rootSaga from "./rootSaga";
 import moviesReducer from "./features/movies/moviesSlice";
+import peopleReducer from "./features/people/peopleSlice";
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
     reducer: {
         movies: moviesReducer,
+        people: peopleReducer,
     },
     middleware: [sagaMiddleware],
 });
