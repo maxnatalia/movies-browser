319be3cc1c1473a11068bcfb6687dd355527b529
maxnatalia <Nataliamazur1988@gmail.com>
2022-11-30 17:59:51 +0100

    refactoring api.js

--- src/features/movies/api.js
+++ src/features/movies/api.js
@@ -1,20 +1,10 @@
 import { apiKey, apiMovieDetails, apiUrl } from "../../apiData";
 
-export const APIpopularMoviesUrl = page =>
+export const getPopularMovies = page =>
   `${apiUrl}/movie/popular?api_key=${apiKey}&page=${page}`;
 
-export const APIsearchMovieUrl = (query, page) =>
+export const getSearchByQuery = (query, page) =>
   `${apiUrl}/search/movie?api_key=${apiKey}&query=${query}&page=${page}`;
-  
-// export const getPopularMovies = async (page) => {
-//     const response = await fetch(`${apiUrl}movie/popular?api_key=${apiKey}&language=en-US&page=${page}`);
-
-//     if (!response.ok) {
-//         throw new Error(response.statusText);
-//     }
-
-//     await response.json();
-// };
 
 export const getGenres = async () => {
     const response = await fetch(`${apiUrl}genre/movie/list?api_key=${apiKey}`);
@@ -28,18 +18,6 @@ export const getGenres = async () => {
     return genres;
 };
 
-// export const getMoviesByQuery = async (query, page) => {
-//     const response = await fetch(`${apiUrl}search/movie?api_key=${apiKey}&language=en-US&page=${page}&query=${query}`);
-
-//     if (!response.ok) {
-//         throw new Error(response.statusText);
-//     }
-
-//     await response.json()
-
-// };
-
-
 export const getMovieDetails = async (id) => {
     const response = await fetch(`${apiUrl}${apiMovieDetails}${id}?api_key=${apiKey}`);
 
@@ -51,11 +29,3 @@ export const getMovieDetails = async (id) => {
 
     return movieDetails;
 };
-
-// export const getMovies = (query, page) => {
-//     if (!query) {
-//         return getMoviesByQuery(query, page);
-//     }
-
-//     return getPopularMovies(page);
-// };
