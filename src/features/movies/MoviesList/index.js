1613c593801d61408e25e51470f2033c416db37c
maxnatalia <Nataliamazur1988@gmail.com>
2022-11-30 15:39:35 +0100

    added query logic to MoviesList

--- src/features/movies/MoviesList/index.js
+++ src/features/movies/MoviesList/index.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchMovies, fetchGenres, selectError, selectLoading, selectMovies, selectGenres, selectQuery } from "../moviesSlice";
+import { fetchMovies, fetchGenres, selectError, selectLoading, selectMovies, selectGenres, fetchMoviesSearch, selectTotalResults } from "../moviesSlice";
 import {
   MainWrapper,
   TilesContainer,
@@ -24,13 +24,18 @@ import Loading from "../../../common/Loading";
 import Pagination from "../../../common/Pagination";
 import NoResults from "../../../common/NoResults";
 import { toMovieDetails } from "../../../routes";
+import { useQueryParameter } from "../../../common/Navigation/Search/queryParametersHooks";
+import { queryParameters } from "../../../common/Navigation/Search/queryParameters";
 
 const MoviesList = ({ insideDetails }) => {
   const movies = useSelector(selectMovies);
   const genres = useSelector(selectGenres);
   const loading = useSelector(selectLoading);
   const error = useSelector(selectError);
-  const query = useSelector(selectQuery);
+  const query = useQueryParameter(queryParameters.search);
+  const totalResults = useSelector(selectTotalResults);
+  const paramsPage = +useQueryParameter(queryParameters.page);
+  const page = paramsPage === 0 ? 1 : paramsPage;
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -40,8 +45,10 @@ const MoviesList = ({ insideDetails }) => {
   });
 
   useEffect(() => {
-    dispatch(fetchMovies());
-  }, [dispatch]);
+    query
+      ? dispatch(fetchMoviesSearch({ query, page }))
+      : dispatch(fetchMovies({ query, page }));
+  }, [dispatch, query, page]);
 
   if (error) {
     return <Error />;
@@ -51,13 +58,13 @@ const MoviesList = ({ insideDetails }) => {
     <MainWrapper insideDetails={insideDetails}>
       {error && <Error />}
       {loading && <Loading loadingMessage={query ? `Search results for "${query}"` : ""} />}
-      {movies.total_results === 0 && <NoResults noResultMessage={`Sorry, there are no results for “${query}”`} />}
+      {totalResults === 0 ? <NoResults noResultMessage={`Sorry, there are no results for “${query}”`} /> : error && <Error />}
 
-      {movies.total_results > 1 && !loading &&
+      {totalResults > 1 && !loading &&
         <>
-          <Header>{query ? `Search results for "${query}" (${movies.total_results})` : `Popular movies`}</Header>
+          <Header>{query ? `Search results for "${query}" (${totalResults})` : `Popular movies`}</Header>
           <TilesContainer insideDetails={insideDetails}>
-            {movies.results.map((movie) => (
+            {movies.map((movie) => (
               <StyledLink key={movie.id} to={toMovieDetails({ id: movie.id })}>
                 <TileMovie>
                   <ImageWrapper>
