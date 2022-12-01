a31b001d30c1b8d4e5f1a8277f2bb3d2a19854a0
Patryk Krawczyk <patrick36212@gmail.com>
2022-12-01 20:06:29 +0100

    change paths for routes

--- src/features/movies/MoviesList/index.js
+++ src/features/movies/MoviesList/index.js
@@ -23,7 +23,7 @@ import Error from "../../../common/Error";
 import Loading from "../../../common/Loading";
 import Pagination from "../../../common/Pagination";
 import NoResults from "../../../common/NoResults";
-import { toMovieDetails } from "../../../routes";
+import Navigation from "../../../common/Navigation";
 
 const MoviesList = ({ insideDetails }) => {
   const movies = useSelector(selectMovies);
@@ -44,49 +44,52 @@ const MoviesList = ({ insideDetails }) => {
   }, [dispatch]);
 
   return (
-    <MainWrapper insideDetails={insideDetails}>
-      {error && <Error />}
-      {loading && <Loading loadingMessage={query ? `Search results for "${query}"` : ""} />}
-      {movies.total_results === 0 && <NoResults noResultMessage={`Sorry, there are no results for “${query}”`} />}
+    <>
+      <Navigation />
+      <MainWrapper insideDetails={insideDetails}>
+        {error && <Error />}
+        {loading && <Loading loadingMessage={query ? `Search results for "${query}"` : ""} />}
+        {movies.total_results === 0 && <NoResults noResultMessage={`Sorry, there are no results for “${query}”`} />}
 
-      {movies.total_results > 1 && !loading &&
-        <>
-          <Header>{query ? `Search results for "${query}" (${movies.total_results})` : `Popular movies`}</Header>
-          <TilesContainer insideDetails={insideDetails}>
-            {movies.results.map((movie) => (
-              <StyledLink key={movie.id} to={toMovieDetails({ id: movie.id })}>
-                <TileMovie>
-                  <ImageWrapper>
-                    {movie.poster_path ?
-                      <Image src={`https://image.tmdb.org/t/p/w400${movie.poster_path}`} alt="Movie poster"/> :
-                      <Image />
-                    }
-                  </ImageWrapper>
-                  <ContentWrapper>
-                    <Title>{movie.title}</Title>
-                    {movie.release_date ? <StyledDate>{movie.release_date.slice(0, 4)}</StyledDate> : ""}
-                    {movie.genre_ids ? (
-                      <TagsWrapper>
-                        {movie.genre_ids.map((id) => (
-                          <Tag key={id}>{genres.find((genre) => genre.id === id).name}</Tag>
-                        ))}
-                      </TagsWrapper>
-                    ) : (
-                      ""
-                    )}
-                    <RatingWrapper>
-                      <StarIcon />
-                      <Rate>{movie.vote_average}</Rate>
-                      <Votes>{movie.vote_count} votes</Votes>
-                    </RatingWrapper>
-                  </ContentWrapper>
-                </TileMovie>
-              </StyledLink>
-            ))}
-          </TilesContainer>
-          <Pagination />
-        </>}
-    </MainWrapper>
+        {movies.total_results > 1 && !loading &&
+          <>
+            <Header>{query ? `Search results for "${query}" (${movies.total_results})` : `Popular movies`}</Header>
+            <TilesContainer insideDetails={insideDetails}>
+              {movies.results.map((movie) => (
+                <StyledLink key={movie.id} to={`/movie/${movie.id}`}>
+                  <TileMovie >
+                    <ImageWrapper>
+                      {movie.poster_path ?
+                        <Image src={`https://image.tmdb.org/t/p/w400${movie.poster_path}`} alt="Movie poster" /> :
+                        <Image />
+                      }
+                    </ImageWrapper>
+                    <ContentWrapper>
+                      <Title>{movie.title}</Title>
+                      {movie.release_date ? <StyledDate>{movie.release_date.slice(0, 4)}</StyledDate> : ""}
+                      {movie.genre_ids ? (
+                        <TagsWrapper>
+                          {movie.genre_ids.map((id) => (
+                            <Tag key={id}>{genres.find((genre) => genre.id === id).name}</Tag>
+                          ))}
+                        </TagsWrapper>
+                      ) : (
+                        ""
+                      )}
+                      <RatingWrapper>
+                        <StarIcon />
+                        <Rate>{movie.vote_average}</Rate>
+                        <Votes>{movie.vote_count} votes</Votes>
+                      </RatingWrapper>
+                    </ContentWrapper>
+                  </TileMovie>
+                </StyledLink>
+              ))}
+            </TilesContainer>
+            <Pagination />
+          </>}
+      </MainWrapper>
+    </>
   )
 };
 
