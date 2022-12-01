9dd78d8f7c5a601c5d1cd5ed8701b78d8d923b66
Patryk Krawczyk <patrick36212@gmail.com>
2022-12-01 20:05:17 +0100

    make routes elemnts

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,22 @@
-export const toPopularMovies = () => "/movies";
-export const toMovieDetails = ({ id } = { id: ":id" }) => `/movie-details/${id}`;
-export const toPeople = () => "/people";
-export const toPersonDetails = ({ id } = { id: ":id" }) => `/person-details/${id}`;
+import { createHashRouter, createRoutesFromElements, Navigate, Route } from "react-router-dom";
+import MovieDetails from "./features/movies/MovieDetails";
+import MoviesList from "./features/movies/MoviesList";
+import PeopleList from "./features/people/PeopleList";
+import PersonDetails from "./features/people/PersonDetails";
+
+export const toPopularMovies = "/movies";
+export const toMovieDetails = "/movie/:id";
+export const toPeople = "/people";
+export const toPersonDetails = "/person/:id";
+
+export const router = createHashRouter(
+    createRoutesFromElements(
+        <>
+            <Route path={toPersonDetails} element={<PersonDetails />} />
+            <Route path={toMovieDetails} element={<MovieDetails />} />
+            <Route path={toPopularMovies} element={<MoviesList />} />
+            <Route path={toPeople} element={<PeopleList />} />
+            <Route path="/" element={<Navigate replace to={toPopularMovies} />} />
+        </>
+    )
+)
