99221b1f8fd8668fb6c2bb5e87ed26d29979aee8
Patryk Krawczyk <patrick36212@gmail.com>
2022-12-04 11:02:25 +0100

    add funcion with navigate to url search params

--- src/common/Navigation/Search/index.js
+++ src/common/Navigation/Search/index.js
@@ -1,21 +1,34 @@
-import React from "react";
-import { useDispatch } from "react-redux";
-import { setQuery } from "../../../features/movies/moviesSlice";
-import { SearchIcon, SearchInput, SearchWrapper } from "./styled";
 import debounce from "lodash.debounce";
+import React from "react";
+import { useLocation, useNavigate } from "react-router";
+import { createSearchParams } from "react-router-dom";
+import { queryParamName, useQueryParams } from "../../../features/movies/ulrSearchParams";
+import { toPopularMovies } from "../../../routes";
+import { SearchIcon, SearchInput, SearchWrapper } from "./styled";
 
 const Search = () => {
 
-    const dispatch = useDispatch();
-    const handleChange = debounce((input) => {
-        dispatch(setQuery(input));
+    const location = useLocation();
+    const navigate = useNavigate();
+    const [queryParam, setQueryParamToUrl] = useQueryParams();
+
+    const handleQueryOnChange = debounce(({ target }) => {
+        if (location.pathname !== toPopularMovies) {
+            navigate({
+                pathname: toPopularMovies,
+                search: createSearchParams({ [queryParamName]: target.value }).toString()
+            });
+        } else {
+            setQueryParamToUrl(target.value);
+        };
     }, 1000);
 
     return (
         <SearchWrapper>
             <SearchIcon />
             <SearchInput
-                onChange={(event) => handleChange(event.target.value)}
+                onChange={handleQueryOnChange}
+                defaultValue={queryParam || ""}
                 type="text"
                 placeholder="Search for movies..."
             />
