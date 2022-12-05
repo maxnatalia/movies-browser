95c118d57409aeba41c79b5262fd66509106c0b5
Patryk Krawczyk <patrick36212@gmail.com>
2022-12-05 18:04:41 +0100

    debounced fn for controlled input

--- src/common/Navigation/Search/index.js
+++ src/common/Navigation/Search/index.js
@@ -1,5 +1,5 @@
 import debounce from "lodash.debounce";
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router";
 import { createSearchParams } from "react-router-dom";
 import { queryParamName, useQueryParams } from "../../../features/movies/ulrSearchParams";
@@ -11,24 +11,31 @@ const Search = () => {
     const location = useLocation();
     const navigate = useNavigate();
     const [queryParam, setQueryParamToUrl] = useQueryParams();
+    const [inputValue, setInputValue] = useState(queryParam);
 
-    const handleQueryOnChange = debounce(({ target }) => {
-        if (location.pathname !== toPopularMovies) {
-            navigate({
-                pathname: toPopularMovies,
-                search: createSearchParams({ [queryParamName]: target.value }).toString()
-            });
-        } else {
-            setQueryParamToUrl(target.value);
-        };
-    }, 1000);
+    const handleQueryOnChange = useCallback(
+        debounce(value => {
+            if (location.pathname !== toPopularMovies) {
+                navigate({
+                    pathname: toPopularMovies,
+                    search: createSearchParams({ [queryParamName]: value }).toString()
+                });
+            } else {
+                setQueryParamToUrl(value);
+            };
+        }, 1000), 
+        []);
+
+    useEffect(() => {
+        handleQueryOnChange(inputValue);
+    }, [inputValue, handleQueryOnChange])
 
     return (
         <SearchWrapper>
             <SearchIcon />
             <SearchInput
-                onChange={handleQueryOnChange}
-                defaultValue={queryParam || ""}
+                onChange={({ target }) => setInputValue(target.value)}
+                value={inputValue || ""}
                 type="text"
                 placeholder="Search for movies..."
             />
