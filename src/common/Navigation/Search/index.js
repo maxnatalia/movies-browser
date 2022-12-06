3e781423de3c751f9ea821284e01c26a60abc0bc
Patryk Krawczyk <patrick36212@gmail.com>
2022-12-06 10:47:24 +0100

    add placeholder for all cases

--- src/common/Navigation/Search/index.js
+++ src/common/Navigation/Search/index.js
@@ -1,23 +1,24 @@
 import debounce from "lodash.debounce";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router";
 import { createSearchParams } from "react-router-dom";
 import { queryParamName, useQueryParams } from "../../../features/movies/ulrSearchParams";
-import { toPeople, toPopularMovies } from "../../../routes";
+import { toMovieDetails, toPeople, toPersonDetails, toPopularMovies } from "../../../routes";
 import { SearchIcon, SearchInput, SearchWrapper } from "./styled";
 
 const Search = () => {
     const location = useLocation();
     const navigate = useNavigate();
+    const [placeholder, setPlaceholder] = useState("");
     const [queryParam, setQueryParamToUrl] = useQueryParams();
 
     const handleQueryOnChange = debounce(({ target }) => {
-        if (location.pathname.split("/")[1] === `movie`) {
+        if (location.pathname.split("/")[1] === toMovieDetails.split("/")[1]) {
             navigate({
                 pathname: toPopularMovies,
                 search: createSearchParams({ [queryParamName]: target.value }).toString()
             });
-        } else if (location.pathname.split("/")[1] === `person`) {
+        } else if (location.pathname.split("/")[1] === toPersonDetails.split("/")[1]) {
             navigate({
                 pathname: toPeople,
                 search: createSearchParams({ [queryParamName]: target.value }).toString()
@@ -28,6 +29,14 @@ const Search = () => {
         };
     }, 1000);
 
+    useEffect(() => {
+        if (location.pathname.split("/")[1] === toMovieDetails.split("/")[1] || location.pathname.split("/")[1] === toPopularMovies.split("/")[1]) {
+            setPlaceholder(`Search for ${toPopularMovies.split("/")[1]}...`);
+        } else {
+            setPlaceholder(`Search for ${toPeople.split("/")[1]}...`);
+        }
+    }, [location.pathname])
+
     return (
         <SearchWrapper>
             <SearchIcon />
@@ -35,7 +44,7 @@ const Search = () => {
                 onChange={handleQueryOnChange}
                 defaultValue={queryParam || ""}
                 type="text"
-                placeholder="Search for movies..."
+                placeholder={placeholder}
             />
         </SearchWrapper>
     );
