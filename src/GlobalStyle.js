2bf0186230ed427156dae09cdbc2aead15c0a3b0
golosgit <107067096+golosgit@users.noreply.github.com>
2022-12-06 10:37:54 +0100

    added overflow-wrap to globalstyle to implement proper word wrapping

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -15,5 +15,6 @@ export const GlobalStyle = createGlobalStyle`
     body {
         font-family: 'Poppins', sans-serif; 
         background-color: ${({ theme }) => theme.color.whisper};
+        overflow-wrap: anywhere;
     }
 `;
