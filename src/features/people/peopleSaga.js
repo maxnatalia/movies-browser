b132ceba93751fb5ba7a7b4712a6c1e73df32e42
Patryk Krawczyk <patrick36212@gmail.com>
2022-12-06 09:34:15 +0100

    fn for api

--- src/features/people/peopleSaga.js
+++ src/features/people/peopleSaga.js
@@ -1,12 +1,24 @@
-import { takeLatest, put, call, delay } from "redux-saga/effects";
-import { fetchPeopleError, fetchPeopleSuccess, fetchPeople } from "./peopleSlice";
+import { takeLatest, put, call, delay, select } from "redux-saga/effects";
+import {
+    fetchPeopleError,
+    fetchPeopleSuccess,
+    fetchPeople,
+    selectQuery,
+    selectPage,
+    setLoadingState,
+    setQuery,
+    setPage
+} from "./peopleSlice";
 import { getPeople } from "./api";
 
 function* fetchPeopleHandler() {
+    const page = yield select(selectQuery);
+    const query = yield select(selectPage)
+
     try {
-        const data = yield call(getPeople);
-        const people = yield data.results;
         yield delay(500);
+        const data = yield call(getPeople, query, page);
+        const people = yield data.results;
         yield put(
             fetchPeopleSuccess({
                 people,
@@ -15,8 +27,16 @@ function* fetchPeopleHandler() {
     } catch (error) {
         yield put(fetchPeopleError());
     }
-}
+};
+
+function* setOnChangeHandler() {
+    yield put(setLoadingState());
+    yield delay(500);
+    yield put(fetchPeople());
+};
 
 export function* watchFetchPopularPeople() {
+    yield takeLatest(setQuery.type, setOnChangeHandler)
+    yield takeLatest(setPage.type, setOnChangeHandler);
     yield takeLatest(fetchPeople.type, fetchPeopleHandler);
 }
