ed3585d4169e5f49154c959e450187412087d71c
Patryk Krawczyk <patrick36212@gmail.com>
2022-12-09 17:15:17 +0100

    movie details to one slice & saga

--- src/features/movies/moviesSaga.js
+++ src/features/movies/moviesSaga.js
@@ -1,17 +1,21 @@
 import { takeLatest, put, call, delay, select } from "redux-saga/effects";
-import { 
-    fetchError, 
-    fetchMoviesSuccess, 
-    fetchMovies, 
-    fetchGenres, 
-    fetchGenresSuccess, 
-    selectQuery, 
-    setQuery, 
-    selectPage, 
-    setPage, 
-    setLoadingState
+import {
+    fetchError,
+    fetchMoviesSuccess,
+    fetchMovies,
+    fetchGenres,
+    fetchGenresSuccess,
+    fetchMovieDetailsSuccess,
+    fetchMovieCreditsSuccess,
+    fetchMovieDetails,
+    selectQuery,
+    selectId,
+    setQuery,
+    selectPage,
+    setPage,
+    setLoadingState,
 } from "./moviesSlice";
-import { getGenres, getMovies } from "./api";
+import { getGenres, getMovies, getMovieDetails, getMovieCredits } from "./api";
 
 function* fetchMoviesHandler() {
     const query = yield select(selectQuery);
@@ -29,7 +33,7 @@ function* fetchMoviesHandler() {
     } catch (error) {
         yield put(fetchError());
     }
-}
+};
 
 function* fetchGenresHandler() {
     try {
@@ -39,7 +43,20 @@ function* fetchGenresHandler() {
     } catch (error) {
         yield put(fetchError());
     }
-}
+};
+
+function* fetchMovieDetailsHandler() {
+    try {
+        const id = yield select(selectId);
+        const movie = yield call(getMovieDetails, id);
+        const credits = yield call(getMovieCredits, id);
+        yield delay(500);
+        yield put(fetchMovieDetailsSuccess(movie));
+        yield put(fetchMovieCreditsSuccess(credits));
+    } catch (error) {
+        yield put(fetchError());
+    }
+};
 
 function* setOnChangeHandler() {
     yield put(setLoadingState(true));
@@ -47,9 +64,10 @@ function* setOnChangeHandler() {
     yield put(fetchMovies());
 };
 
-export function* watchFetchPopularMovies() {
+export function* moviesSaga() {
+    yield takeLatest(fetchMovieDetails.type, fetchMovieDetailsHandler);
     yield takeLatest(fetchGenres, fetchGenresHandler);
     yield takeLatest(fetchMovies.type, fetchMoviesHandler);
     yield takeLatest(setQuery.type, setOnChangeHandler);
     yield takeLatest(setPage.type, setOnChangeHandler);
-}
+};
--- src/features/movies/moviesSlice.js
+++ src/features/movies/moviesSlice.js
@@ -5,13 +5,16 @@ const moviesSlice = createSlice({
     initialState: {
         loading: true,
         error: false,
+        query: null,
+        page: 1,
         movies: [],
         genres: [],
-        query: "",
-        page: 1,
+        movieId: "",
+        movieDetails: [],
+        movieCredits: [],
     },
     reducers: {
-        setLoadingState: (state, {payload: boolean}) => {
+        setLoadingState: (state, { payload: boolean }) => {
             state.loading = boolean;
         },
         setQuery: (state, { payload: query }) => {
@@ -37,6 +40,26 @@ const moviesSlice = createSlice({
         fetchGenresSuccess: (state, { payload: genres }) => {
             state.genres = genres;
         },
+        changeMovieId: (state, { payload: id }) => {
+            state.movieId = id;
+        },
+        fetchMovieDetails: (state) => {
+            state.loading = true;
+            state.error = false;
+            state.movieDetails = [];
+        },
+        fetchMovieDetailsSuccess: (state, { payload: movie }) => {
+            state.loading = false;
+            state.movieDetails = movie;
+        },
+        fetchMovieCredits: (state) => {
+            state.loading = true;
+            state.movieCredits = [];
+        },
+        fetchMovieCreditsSuccess: (state, { payload: credits }) => {
+            state.loading = false;
+            state.movieCredits = credits;
+        },
     },
 });
 
@@ -49,7 +72,11 @@ export const {
     fetchMoviesSuccess,
     fetchGenres,
     fetchGenresSuccess,
-
+    changeMovieId,
+    fetchMovieDetails,
+    fetchMovieDetailsSuccess,
+    fetchMovieCredits,
+    fetchMovieCreditsSuccess,
 } = moviesSlice.actions;
 
 export const selectMoviesState = (state) => state.movies;
@@ -59,5 +86,8 @@ export const selectError = (state) => selectMoviesState(state).error;
 export const selectGenres = (state) => selectMoviesState(state).genres;
 export const selectQuery = (state) => selectMoviesState(state).query;
 export const selectPage = (state) => selectMoviesState(state).page;
+export const selectId = (state) => selectMoviesState(state).movieId;
+export const selectMovieDetails = (state) => selectMoviesState(state).movieDetails;
+export const selectMovieCredits = (state) => selectMoviesState(state).movieCredits;
 
 export default moviesSlice.reducer;
