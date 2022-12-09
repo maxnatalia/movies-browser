7794450e804d5ec73a287a4b5eb4cfd506690bf0
Patryk Krawczyk <patrick36212@gmail.com>
2022-12-09 17:16:05 +0100

    change store for 2 reducers

--- src/core/store.js
+++ src/core/store.js
@@ -2,18 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "@redux-saga/core";
 import rootSaga from "./rootSaga";
 import moviesReducer from "../features/movies/moviesSlice";
-import movieDetailsReducer from "../features/movies/MovieDetails/movieDetailsSlice";
 import peopleReducer from "../features/people/peopleSlice";
-import personDetailsReducer from "../features/people/PersonDetails/personDetailsSlice";
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
     reducer: {
         movies: moviesReducer,
-        movieDetails: movieDetailsReducer,
         people: peopleReducer,
-        personDetails: personDetailsReducer,
     },
     middleware: [sagaMiddleware],
 });
