07a0fa7c7991662327b6e02a4296a7c256eb5f24
golosgit <107067096+golosgit@users.noreply.github.com>
2022-12-09 21:09:41 +0100

    deleted unnecessarry import

--- src/features/movies/MoviesList/index.js
+++ src/features/movies/MoviesList/index.js
@@ -2,7 +2,6 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
   fetchMovies,
-  fetchGenres,
   selectError,
   selectLoading,
   selectMovies,
